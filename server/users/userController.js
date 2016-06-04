d49e63db3580127e30dd2cd4d71ca46debc2cd2d
Andrew Vye <a.vye26@gmail.com>
2016-06-03 19:04:30 -0700

    (feat) Remove unused userController function

--- server/users/userController.js
+++ server/users/userController.js
@@ -13,7 +13,7 @@ module.exports = {
 	// hybrid signin/signup
 	signin: function(userObj, cb) {
 		// userObj = {'roomname': {users: {'stuff': [Object]}}}
-		console.log(userObj);
+		console.log('this is the dfsdf', userObj);
 		var username = userObj.username;
 		console.log(username);
 
@@ -103,67 +103,4 @@ module.exports = {
 			cb(error);
 		});
 	},
-
-	// Seperate signin/signup features, for use later
-	// signin: function(req, res, cb) {
-	// 	var username = req.body.username;
-
-	// 	findUser({username: username})
-	// 		.then(function(user) {
-	// 			if (!user) {
-	// 				cb(new Error('User not found'));
-	// 			} else {
-	// 				return if (foundUser) {
-	// 						var token = jwt.encode(user, 'secret');
-	// 						res.json({token: token});
-	// 					} else {
-	// 						return cb(new Error('no user'));
-	// 					}
-	// 				}
-	// 			})
-	// 		.fail(function(error){
-	// 			cb(error);
-	// 		});
-	// },
-
-	// signupUser: function(req, res, cb) {
-	// 	var username = req.body.username;
-
-	// 	findUser({username: username})
-	// 		.then(function(user) {
-	// 			if (user) {
-	// 				cb(new Error('User already exists'));
-	// 			} else {
-	// 				return createUser({username:username});
-	// 			}
-	// 		})
-	// 		.then(function(user) {
-	// 			if (!user) {
-	// 				cb(new Error('User not found'));
-	// 			} else {
-	// 				return
-	// 					if (foundUser) {
-	// 						var token = jwt.encode(user, 'secret');
-	// 						res.json({token: token});
-	// 					} else {
-	// 						return cb(new Error('no user'));
-	// 					}
-	// 			}
-	// 		})
-	// 		.fail(function(error){
-	// 			cb(error);
-	// 		});
-	// }
-
-	//Keep uncommented until usaged first
-	// allusers: function(req, res, cb) {
-	// 	findAll()
-	// 	.then(function(allUsers){
-	// 		if (allUsers){
-	// 			return allUsers;
-	// 		} else {
-	// 			return cb(new Error('users not found'));
-	// 		}
-	// 	});
-	// }
-}
+};
