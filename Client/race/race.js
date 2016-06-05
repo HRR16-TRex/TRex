1bf8b6ef5ebda3382e9063e3f362450c0e715afa
Andrew Vye <a.vye26@gmail.com>
2016-06-04 18:54:10 -0700

    (fix) Socket returned to just io()

--- Client/race/race.js
+++ Client/race/race.js
@@ -233,7 +233,7 @@ angular.module("app.race", ['ngRoute'])
   
     // var socket = io.connect('https://trex-timer.herokuapp.com:80/');
     // var socket = io.connect(window.location.hostname);
-    var socket = io.connect(window.location.hostname);
+    var socket = io();
     
     var on = function (eventName, callback) {
       socket.on(eventName, function () {  
